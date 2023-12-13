991241df1fb6b4e00676a2155446bb6fe28a3831
TaillepierreN <taillepierren@gmail.com>
2023-12-13 04:00:45 -0800

    move super_simple_shell to new file

---
+++ super_simple_shell/super_simple_shell.c
@@ -0,0 +1,18 @@
+#include "shell_header.h"
+
+int main(void)
+{
+	char *usrInput;
+
+	while (1)
+	{
+		printf("SS Shell ");
+		usrInput = get_user_input();
+
+		if (usrInput == NULL)
+			break;
+	
+	}
+	printf("\n");
+	return (0);
+}
